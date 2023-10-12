6c5dafd030a9188eccc8fb0c866f7c85053ca95d
Adam Pekny <peknyadam9@gmail.com>
2023-10-12 14:44:49 +0200

    Utility functions for db conn and migration added

--- backend/router.go
+++ backend/router.go
@@ -1,22 +1,5 @@
 package api
 
-import (
-	"fmt"
-
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-
-	models "github.com/AdamPekny/IIS/backend/models"
-)
-
 func Router() {
-	db, _ := conn()
-	fmt.Print(db.Table("information_schema.tables"))
-	db.AutoMigrate(&models.User{}, &models.UserType{})
-}
 
-func conn() (*gorm.DB, error) {
-	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	return db, err
 }
---
+++ backend/utils/utils.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"github.com/AdamPekny/IIS/backend/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Conn() (*gorm.DB, error) {
+	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return db, err
+}
+
+func Migrate_all() {
+	db, _ := Conn()
+
+	// Migrate User models
+	db.AutoMigrate(&models.User{}, &models.UserType{})
+
+	// Migrate Vehicle models
+	db.AutoMigrate(&models.Vehicle{}, &models.VehicleType{})
+}
