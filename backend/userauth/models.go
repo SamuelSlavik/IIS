f1ec62a8870d05b66eb9470cd4bedc98a33b94cd
Adam Pekny <peknyadam9@gmail.com>
2023-10-16 19:17:09 +0200

    Utils modified

--- backend/userauth/models.go
+++ backend/userauth/models.go
@@ -8,16 +8,15 @@ import (
 
 type User struct {
 	gorm.Model
-	ID            uint   `gorm:"primaryKey;autoIncrement;not null"`
-	FirstName     string `gorm:"not null"`
-	LastName      string `gorm:"not null"`
-	Email         string `gorm:"not null;unique"`
-	Age           uint8  `gorm:"not null"`
-	Password      string `gorm:"not null"`
-	Salt          string `gorm:"not null"`
-	UserTypeRefer uint
-	UserType      UserType  `gorm:"foreignKey:UserTypeRefer"`
-	CreatedAt     time.Time `gorm:"autoCreateTime"`
+	ID        uint      `gorm:"primaryKey;autoIncrement;not null"`
+	FirstName string    `gorm:"not null"`
+	LastName  string    `gorm:"not null"`
+	Email     string    `gorm:"not null;unique"`
+	Age       uint8     `gorm:"not null"`
+	Password  string    `gorm:"not null"`
+	Salt      string    `gorm:"not null"`
+	UserType  UserType  `gorm:"foreignKey:CodeName"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
 }
 
 type UserType struct {
