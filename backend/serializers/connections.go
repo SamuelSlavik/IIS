39966309d3e7cfb77d340847e9b7b80516fa1f4f
Jakub Kontrik <jakubkontrik@gmail.com>
2023-11-24 02:27:40 +0100

    Add assignment into connection

--- backend/serializers/connections.go
+++ backend/serializers/connections.go
@@ -38,7 +38,16 @@ type ConnectionCreateSerializer struct {
 	Dirrection    bool `binding:"required"`
 	DriverID      *uint
 	ValidatorErrs []validators.ValidatorErr
-	NumberOfDays  int `binding:"required"`
+	NumberOfDays  int       `binding:"required"`
+	ArrivalTime   time.Time //neplnit z fe
+}
+
+type ConnectionAssignSerializer struct {
+	DriverID      *uint
+	VehicleReg    *string
+	DepartureTime string    //neplnit z fe
+	ArrivalTime   time.Time //neplnit z fe
+	ValidatorErrs []validators.ValidatorErr
 }
 
 func (conn *ConnectionCreateSerializer) Valid() bool {
@@ -48,12 +57,7 @@ func (conn *ConnectionCreateSerializer) Valid() bool {
 	if len(conn.ValidatorErrs) != 0 {
 		return false
 	}
-	validators.Vehicle_availability(conn.VehicleReg, conn.DepartureTime, conn.NumberOfDays, &conn.ValidatorErrs)
-	validators.Driver_availability(conn.DriverID, conn.DepartureTime, conn.NumberOfDays, &conn.ValidatorErrs)
-	return len(conn.ValidatorErrs) == 0
 
-}
-func (conn ConnectionCreateSerializer) CreateModel() (connection_model []models.Connection) {
 	line := models.Line{}
 	utils.DB.Preload("Segments").First(&line, "name=?", conn.LineName)
 	var duration time.Duration
@@ -61,17 +65,38 @@ func (conn ConnectionCreateSerializer) CreateModel() (connection_model []models.
 		fmt.Print(segment.Time)
 		duration += time.Minute * time.Duration(segment.Time)
 	}
+
+	dep_time, _ := time.Parse("2006-01-02 15:04:05", conn.DepartureTime)
+	conn.ArrivalTime = dep_time.Add(duration)
+	validators.Vehicle_availability(conn.VehicleReg, conn.DepartureTime, conn.ArrivalTime, conn.NumberOfDays, &conn.ValidatorErrs)
+	validators.Driver_availability(conn.DriverID, conn.DepartureTime, conn.ArrivalTime, conn.NumberOfDays, &conn.ValidatorErrs)
+	return len(conn.ValidatorErrs) == 0
+
+}
+func (conn *ConnectionAssignSerializer) Valid() bool {
+	validators.Vehicle_registration_validator(conn.VehicleReg, &conn.ValidatorErrs)
+	validators.Driver_id_validator(conn.DriverID, &conn.ValidatorErrs)
+	if len(conn.ValidatorErrs) != 0 {
+		return false
+	}
+	validators.Vehicle_availability(conn.VehicleReg, conn.DepartureTime, conn.ArrivalTime, 1, &conn.ValidatorErrs)
+	validators.Driver_availability(conn.DriverID, conn.DepartureTime, conn.ArrivalTime, 1, &conn.ValidatorErrs)
+	return len(conn.ValidatorErrs) == 0
+}
+
+func (conn ConnectionCreateSerializer) CreateModel() (connection_model []models.Connection) {
 	dep_time, _ := time.Parse("2006-01-02 15:04:05", conn.DepartureTime) //todo lolik error
 	for i := 0; i < int(conn.NumberOfDays); i++ {
 		connection_model = append(connection_model, models.Connection{
 			LineName:            conn.LineName,
 			DepartureTime:       dep_time,
-			ArrivalTime:         dep_time.Add(duration),
+			ArrivalTime:         conn.ArrivalTime,
 			VehicleRegistration: conn.VehicleReg,
 			Dirrection:          conn.Dirrection,
 			DriverID:            conn.DriverID,
 		})
 		dep_time = dep_time.Add(time.Hour * 24)
+		conn.ArrivalTime = conn.ArrivalTime.Add(time.Hour * 24)
 	}
 	return
 }
