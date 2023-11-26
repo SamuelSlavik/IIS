dc633a9b03520e4942591a099225f005cd609476
Jakub Kontrik <jakubkontrik@gmail.com>
2023-11-26 23:40:18 +0100

    fix push

--- backend/router.go
+++ backend/router.go
@@ -52,7 +52,7 @@ func Router() *gin.Engine {
 
 	router.GET("/api/stops", views.ListStops)
 	router.GET("/api/stops/get/:id", views.GetStop)
-	//router.DELETE("/api/stops/delete/:id", views.DeleteStop)
+	router.DELETE("/api/stops/delete/:id", views.DeleteStop)
 	router.PUT("/api/stops/edit/:id", views.EditStop)
 	router.POST("/api/stops/create", views.CreateStop)
 
