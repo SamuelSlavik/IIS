b43cd6e2622102dbb698aef1f450be4a8fc1357d
Jakub Kontrik <jakubkontrik@gmail.com>
2023-11-26 23:11:51 +0100

    Add active status for stops

--- backend/serializers/stops.go
+++ backend/serializers/stops.go
@@ -1,6 +1,9 @@
 package serializers
 
-import "github.com/AdamPekny/IIS/backend/models"
+import (
+	"github.com/AdamPekny/IIS/backend/models"
+	"github.com/AdamPekny/IIS/backend/utils"
+)
 
 type StopSerializer struct {
 	ID     uint
@@ -17,10 +20,18 @@ type StopCreateRequest struct {
 	Name string `binding:"required"`
 }
 
-func (s *StopSerializer) FromModel(stop models.Stop) {
+func (s *StopSerializer) FromModel(stop models.Stop) error {
 	s.ID = stop.ID
 	s.Name = stop.Name
-	s.Active = true
-
-	// TODO - implement active status
+	var segments []models.Segment
+	result := utils.DB.Where("stop_name1 = ? OR stop_name2 = ?", stop.Name, stop.Name).Find(&segments)
+	if result.Error != nil {
+		return result.Error
+	}
+	if len(segments) > 0 {
+		s.Active = true
+	} else {
+		s.Active = false
+	}
+	return nil
 }
