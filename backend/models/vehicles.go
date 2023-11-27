d729d06806b07a5c27de35bedadcb804b4c32206
Adam Pekny <peknyadam9@gmail.com>
2023-11-27 21:27:54 +0100

    added stuff

--- backend/models/vehicles.go
+++ backend/models/vehicles.go
@@ -4,7 +4,6 @@ type Vehicle struct {
 	Registration    string `gorm:"primaryKey;unique;not null"`
 	Capacity        uint   `gorm:"not null"`
 	Brand           string
-	ImageData       []byte
 	VehicleTypeName string
 	VehicleType     VehicleType         `gorm:"foreignKey:VehicleTypeName;references:Type"`
 	Connections     []Connection        `gorm:"constraint:OnDelete:SET NULL"`
